888d547f31177666306e712aee1f086abd2b57f9
javier velez <javier.velez.reyes@gmail.com>
2025-01-27 12:51:00 +0100

    Minor changes

--- code/04. Intarface/Case.B/wshop.check.js
+++ code/04. Intarface/Case.B/wshop.check.js
@@ -1,10 +1,10 @@
-import Checable from './role.check.js'
+import Checkable from './role.check.js'
 
 function WShop () {
 
   function execute (vehicles) {
     for (let vehicle of vehicles) {
-      let check = Checable (vehicle)
+      let check = Checkable (vehicle)
       check.start ()
       check.stop  ()
     }
